fc2012e473e6b3ba35bcc5db885092d85aaf2491
lonelydomino <lonelydomino@icloud.com>
2025-01-09 02:27:00 -0800

    Updated cta.

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -4,8 +4,8 @@ import "./CTA.css";
 const CTA = () => {
   return (
     <div className="cta-container">
-      <h1>Join Our Community Today!</h1>
-      <p>Discover the benefits of being a part of something great. Don’t miss out!</p>
+      <h1>Take action today!</h1>
+      <p>Discover how you can help save our environment!</p>
       <button className="cta-button" onClick={() => alert("Button Clicked!")}>
         Get Started
       </button>
